13fbf5ee7fc3dde53749649733c6ea1f89c117a1
ahmatjan74 <ahmatjan74@163.com>
2022-07-05 16:12:57 +0800

    unet

--- python-basic/src/semanticSegmentation/unet/models/unet.py
+++ python-basic/src/semanticSegmentation/unet/models/unet.py
@@ -12,13 +12,13 @@ class _EncoderBlock(nn.Module):
             nn.Conv2d(in_channels, out_channels, kernel_size=3),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3),
+            nn.Conv2d(out_channels, out_channels),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace=True)
         ]
         if dropout:
             layers.append(nn.Dropout())
-        layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+        layers.append(nn.MaxPool2d(kernel_size=2, stride=1))
         self.encode = nn.Sequential(*layers)
 
     def forward(self, x):
@@ -35,7 +35,7 @@ class _DecoderBlock(nn.Module):
             nn.Conv2d(middle_channels, middle_channels, kernel_size=3),
             nn.BatchNorm2d(middle_channels),
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(middle_channels, out_channels, kernel_size=2, stride=2),
+            nn.ConvTranspose2d(middle_channels, out_channels, kernel_size=2, stride=2)
         )
 
     def forward(self, x):
@@ -46,10 +46,12 @@ class UNet(nn.Module):
     def __init__(self, num_classes):
         super(UNet, self).__init__()
         self.enc1 = _EncoderBlock(3, 64)
-        self.enc2 = _EncoderBlock(64, 128)
+        self.enc2 - _EncoderBlock(64, 128)
         self.enc3 = _EncoderBlock(128, 256)
         self.enc4 = _EncoderBlock(256, 512, dropout=True)
+
         self.center = _DecoderBlock(512, 1024, 512)
+
         self.dec4 = _DecoderBlock(1024, 512, 256)
         self.dec3 = _DecoderBlock(512, 256, 128)
         self.dec2 = _DecoderBlock(256, 128, 64)
@@ -59,7 +61,7 @@ class UNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, kernel_size=3),
             nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace=True)
         )
         self.final = nn.Conv2d(64, num_classes, kernel_size=1)
         initialize_weights(self)
@@ -69,10 +71,12 @@ class UNet(nn.Module):
         enc2 = self.enc2(enc1)
         enc3 = self.enc3(enc2)
         enc4 = self.enc4(enc3)
+
         center = self.center(enc4)
         dec4 = self.dec4(torch.cat([center, F.upsample(enc4, center.size()[2:], mode='bilinear')], 1))
-        dec3 = self.dec3(torch.cat([dec4, F.upsample(enc3, dec4.size()[2:], mode='bilinear')], 1))
-        dec2 = self.dec2(torch.cat([dec3, F.upsample(enc2, dec3.size()[2:], mode='bilinear')], 1))
-        dec1 = self.dec1(torch.cat([dec2, F.upsample(enc1, dec2.size()[2:], mode='bilinear')], 1))
+        dec3 = self.dec3(torch.cat([dec4, F.upsample(enc3, dec4.size()[2:], mode='bilinear')]), 1)
+        dec2 = self.dec2(torch.cat([dec3, F.upsample(enc2, dec3.size()[2:], mode='bilinear')]), 1)
+        dec1 = self.dec1(torch.cat([dec2, F.upsample(enc1, dec2.size()[2:], mode='bilinear')]), 1)
+
         final = self.final(dec1)
         return F.upsample(final, x.size()[2:], mode='bilinear')
